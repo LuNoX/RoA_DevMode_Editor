1b27942278304e887465e90915cf5ceb095aff54
LuNoX <maillunox@gmail.com>
2016-10-10 18:38:56 +0200

    Added test_CharacterManager class and functionality

---
+++ src/test_editor/test_CharacterManager.java
@@ -0,0 +1,19 @@
+package test_editor;
+import editor.*;
+import utility.*;
+
+public class test_CharacterManager
+{
+
+	public static void main(String[] args)
+	{
+		IniFileChooser ifc = new IniFileChooser();
+		CharacterManager cm = new CharacterManager(ifc.chooseFile());
+		
+		for (int i = 0; i < cm.getAllMoves()[4].getWindows().length; i++)
+		{
+			System.out.println (cm.getAllMoves()[4].getWindows()[i].getId());
+		}
+	}
+
+}
