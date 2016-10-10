204a13d635dafd9c8c746d8b0c8b24852e056b7e
LuNoX <maillunox@gmail.com>
2016-10-10 19:22:38 +0200

    Added General class

---
+++ src/editor/General.java
@@ -0,0 +1,14 @@
+package editor;
+
+public class General
+{
+	protected String[] groundMovement = null;
+	protected String[] aerialMovement = null;
+	protected String[] jumping = null;
+	protected String[] invincibility = null;
+	
+	public General (int startPosition, int endPosition, String[] content)
+	{
+		
+	}
+}
