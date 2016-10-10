e4005d5f2952331270692f1f87148cab775c3bdb
LuNoX <maillunox@gmail.com>
2016-10-10 12:28:49 +0200

    Added Reader class functionality

---
+++ src/utility/Reader.java
@@ -0,0 +1,54 @@
+package utility;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Reader
+{
+	protected String activeLine = null;
+	protected BufferedReader reader;
+
+	/**
+	 * Erstellt einen neuen Leser anhand einer Datei
+	 * 
+	 * @param file die Datei anhand dessen der Leser erstellt werden soll
+	 */
+	public Reader (File file)
+	{
+		try
+		{
+			reader = new BufferedReader(new FileReader(file));
+			// this.nextLine();
+		}
+		catch (FileNotFoundException e)
+		{
+			System.out.println(e.getLocalizedMessage());
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Liest die nächste Zeile der Datei und speichert sie
+	 */
+	public void nextLine()
+	{
+		try
+		{
+			this.activeLine = reader.readLine();
+		}
+		catch (IOException e)
+		{
+			System.out.println(e.getLocalizedMessage());
+			e.printStackTrace();
+		}
+	}
+
+	public String getActiveLine()
+	{
+		return this.activeLine;
+	}
+
+}
