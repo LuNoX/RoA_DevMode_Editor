a16effea684c03712928cd1936751ea8f573686d
LuNoX <maillunox@gmail.com>
2016-10-11 20:30:22 +0200

    Updated to use WindowManager class structure

--- src/editor/Window.java
+++ src/editor/Window.java
@@ -5,27 +5,17 @@ import java.util.regex.*;
 public class Window
 {
 	protected String id = "0";
-	protected String length = "window_length_" + id + "=";
-	protected String hBoost = "window_h_boost_" + id + "=";
-	protected String vBoost = "window_v_boost_" + id + "=";
-	protected String hBoostConst = "window_h_boost_is_const_" + id + "=";
-	protected String vBoostConst = "window_b_boost_is_const_" + id + "=";
-	protected String hasSfx = "window_has_sfx_" + id + "=";
-	protected String sfx = "window_sfx_" + id + "=";
-	protected String sfxFrame = "window_sfx_frame_" + id + "=";
-	protected String whiffFrames = "window_whiff_frames_" + id + "=";
-	protected String invincibility = "invincibility_" + id + "=";
 	
-	protected float lengthValue = 0;
-	protected float hBoostValue = 0;
-	protected float vBoostValue = 0;
-	protected float hBoostConstValue = 0;
-	protected float vBoostConstValue = 0;
-	protected float hasSfxValue = 0;
-	protected float sfxValue = 0;
-	protected float sfxFrameValue = 0;
-	protected float whiffFramesValue = 0;
-	protected float invincibilityValue = 0;
+	protected float length = 0;
+	protected float hBoost = 0;
+	protected float vBoost= 0;
+	protected float hBoostConst = 0;
+	protected float vBoostConst = 0;
+	protected float hasSfx = 0;
+	protected float sfx = 0;
+	protected float sfxFrame = 0;
+	protected float whiffFrames = 0;
+	protected float invincibility = 0;
 	
 	public Window (String id, String length, String hBoost, String vBoost, String hBoostConst, String vBoostConst, String hasSfx, String sfx, String sfxFrames, String whiffFrames, String invincibility)
 	{
@@ -35,43 +25,43 @@ public class Window
 		
 		m = p.matcher(length);
 		m.find();
-		this.lengthValue = Float.parseFloat(m.group(1));
+		this.length = Float.parseFloat(m.group(1));
 		
 		m = p.matcher(hBoost);
 		m.find();
-		this.hBoostValue = Float.parseFloat(m.group(1));
+		this.hBoost = Float.parseFloat(m.group(1));
 		
 		m = p.matcher(vBoost);
 		m.find();
-		this.vBoostValue = Float.parseFloat(m.group(1));
+		this.vBoost = Float.parseFloat(m.group(1));
 		
 		m = p.matcher(hBoostConst);
 		m.find();
-		this.hBoostConstValue = Float.parseFloat(m.group(1));
+		this.hBoostConst = Float.parseFloat(m.group(1));
 		
 		m = p.matcher(vBoostConst);
 		m.find();
-		this.vBoostConstValue = Float.parseFloat(m.group(1));
+		this.vBoostConst = Float.parseFloat(m.group(1));
 		
 		m = p.matcher(hasSfx);
 		m.find();
-		this.hasSfxValue = Float.parseFloat(m.group(1));
+		this.hasSfx = Float.parseFloat(m.group(1));
 		
 		m = p.matcher(sfx);
 		m.find();
-		this.sfxValue = Float.parseFloat(m.group(1));
+		this.sfx = Float.parseFloat(m.group(1));
 		
 		m = p.matcher(sfxFrames);
 		m.find();
-		this.sfxFrameValue = Float.parseFloat(m.group(1));
+		this.sfxFrame = Float.parseFloat(m.group(1));
 		
 		m = p.matcher(whiffFrames);
 		m.find();
-		this.whiffFramesValue = Float.parseFloat(m.group(1));
+		this.whiffFrames = Float.parseFloat(m.group(1));
 		
 		m = p.matcher(invincibility);
 		m.find();
-		this.invincibilityValue = Float.parseFloat(m.group(1));
+		this.invincibility = Float.parseFloat(m.group(1));
 	}
 
 	public String getId()
@@ -79,103 +69,53 @@ public class Window
 		return id;
 	}
 
-	public String getLength()
+	public float getLength()
 	{
 		return length;
 	}
 
-	public String gethBoost()
+	public float gethBoost()
 	{
 		return hBoost;
 	}
 
-	public String getvBoost()
+	public float getvBoost()
 	{
 		return vBoost;
 	}
 
-	public String gethBoostConst()
+	public float gethBoostConst()
 	{
 		return hBoostConst;
 	}
 
-	public String getvBoostConst()
+	public float getvBoostConst()
 	{
 		return vBoostConst;
 	}
 
-	public String getHasSfx()
+	public float getHasSfx()
 	{
 		return hasSfx;
 	}
 
-	public String getSfx()
+	public float getSfx()
 	{
 		return sfx;
 	}
 
-	public String getSfxFrame()
+	public float getSfxFrame()
 	{
 		return sfxFrame;
 	}
 
-	public String getWhiffFrames()
+	public float getWhiffFrames()
 	{
 		return whiffFrames;
 	}
 
-	public String getInvincibility()
+	public float getInvincibility()
 	{
 		return invincibility;
-	}
-
-	public float getLengthValue()
-	{
-		return lengthValue;
-	}
-
-	public float gethBoostValue()
-	{
-		return hBoostValue;
-	}
-
-	public float getvBoostValue()
-	{
-		return vBoostValue;
-	}
-
-	public float gethBoostConstValue()
-	{
-		return hBoostConstValue;
-	}
-
-	public float getvBoostConstValue()
-	{
-		return vBoostConstValue;
-	}
-
-	public float getHasSfxValue()
-	{
-		return hasSfxValue;
-	}
-
-	public float getSfxValue()
-	{
-		return sfxValue;
-	}
-
-	public float getSfxFrameValue()
-	{
-		return sfxFrameValue;
-	}
-
-	public float getWhiffFramesValue()
-	{
-		return whiffFramesValue;
-	}
-
-	public float getInvincibilityValue()
-	{
-		return invincibilityValue;
 	}
 }
--- src/test_editor/test_CharacterManager.java
+++ src/test_editor/test_CharacterManager.java
@@ -8,11 +8,11 @@ public class test_CharacterManager
 	public static void main(String[] args)
 	{
 		IniFileChooser ifc = new IniFileChooser();
-		CharacterManager cm = new CharacterManager(ifc.chooseFile());
+		CharacterManager cm = new CharacterManager(ifc.chooseFile("C:\\Users\\Timo\\Desktop\\active\\custom_etalus.ini"));
 		
-		for (int i = 0; i < cm.getAllMoves()[4].getWindows().length; i++)
+		for (int i = 0; i < cm.getAllMoves()[4].getWm().getWindows().length; i++)
 		{
-			System.out.println (cm.getAllMoves()[4].getWindows()[i].getSfxValue());
+			System.out.println (cm.getAllMoves()[4].getWm().getWindows()[i].getSfx());
 		}
 	}
 
