5f86162cde3b49579e0f3981c10cffeab67556fc
LuNoX <maillunox@gmail.com>
2016-10-11 21:19:59 +0200

    Cleaned up some code

--- src/editor/Window.java
+++ src/editor/Window.java
@@ -1,14 +1,14 @@
 package editor;
-import java.util.regex.*;
 
+import java.util.regex.*;
 
 public class Window
 {
 	protected String id = "0";
-	
+
 	protected float length = 0;
 	protected float hBoost = 0;
-	protected float vBoost= 0;
+	protected float vBoost = 0;
 	protected float hBoostConst = 0;
 	protected float vBoostConst = 0;
 	protected float hasSfx = 0;
@@ -16,49 +16,49 @@ public class Window
 	protected float sfxFrame = 0;
 	protected float whiffFrames = 0;
 	protected float invincibility = 0;
-	
-	public Window (String id, String length, String hBoost, String vBoost, String hBoostConst, String vBoostConst, String hasSfx, String sfx, String sfxFrames, String whiffFrames, String invincibility)
+
+	public Window(String id, String length, String hBoost, String vBoost, String hBoostConst, String vBoostConst, String hasSfx, String sfx, String sfxFrames, String whiffFrames, String invincibility)
 	{
 		this.id = id;
 		Pattern p = Pattern.compile("\"([^\"]*)\"");
 		Matcher m = null;
-		
+
 		m = p.matcher(length);
 		m.find();
 		this.length = Float.parseFloat(m.group(1));
-		
+
 		m = p.matcher(hBoost);
 		m.find();
 		this.hBoost = Float.parseFloat(m.group(1));
-		
+
 		m = p.matcher(vBoost);
 		m.find();
 		this.vBoost = Float.parseFloat(m.group(1));
-		
+
 		m = p.matcher(hBoostConst);
 		m.find();
 		this.hBoostConst = Float.parseFloat(m.group(1));
-		
+
 		m = p.matcher(vBoostConst);
 		m.find();
 		this.vBoostConst = Float.parseFloat(m.group(1));
-		
+
 		m = p.matcher(hasSfx);
 		m.find();
 		this.hasSfx = Float.parseFloat(m.group(1));
-		
+
 		m = p.matcher(sfx);
 		m.find();
 		this.sfx = Float.parseFloat(m.group(1));
-		
+
 		m = p.matcher(sfxFrames);
 		m.find();
 		this.sfxFrame = Float.parseFloat(m.group(1));
-		
+
 		m = p.matcher(whiffFrames);
 		m.find();
 		this.whiffFrames = Float.parseFloat(m.group(1));
-		
+
 		m = p.matcher(invincibility);
 		m.find();
 		this.invincibility = Float.parseFloat(m.group(1));
