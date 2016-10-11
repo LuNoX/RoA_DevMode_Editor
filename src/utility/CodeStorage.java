a031cdab228821e21ca165635569c72b489ad880
LuNoX <maillunox@gmail.com>
2016-10-11 19:58:19 +0200

    Added CodeStorage class and functionality

---
+++ src/utility/CodeStorage.java
@@ -0,0 +1,58 @@
+package utility;
+
+public class CodeStorage
+{
+	protected String window_length = "window_length_";
+	protected String window_hBoost = "window_h_boost_";
+	protected String window_vBoost = "window_v_boost_";
+	protected String window_hBoostConst = "window_h_boost_is_const_";
+	protected String window_vBoostConst = "window_b_boost_is_const_";
+	protected String window_hasSfx = "window_has_sfx_";
+	protected String window_sfx = "window_sfx_";
+	protected String window_sfxFrame = "window_sfx_frame_";
+	protected String window_whiffFrames = "window_whiff_frames_";
+	protected String window_invincibility = "invincibility_";
+	
+	public String getWindow_length()
+	{
+		return window_length;
+	}
+	public String getWindow_hBoost()
+	{
+		return window_hBoost;
+	}
+	public String getWindow_vBoost()
+	{
+		return window_vBoost;
+	}
+	public String getWindow_hBoostConst()
+	{
+		return window_hBoostConst;
+	}
+	public String getWindow_vBoostConst()
+	{
+		return window_vBoostConst;
+	}
+	public String getWindow_hasSfx()
+	{
+		return window_hasSfx;
+	}
+	public String getWindow_sfx()
+	{
+		return window_sfx;
+	}
+	public String getWindow_sfxFrame()
+	{
+		return window_sfxFrame;
+	}
+	public String getWindow_whiffFrames()
+	{
+		return window_whiffFrames;
+	}
+	public String getWindow_invincibility()
+	{
+		return window_invincibility;
+	}
+	
+	
+}
