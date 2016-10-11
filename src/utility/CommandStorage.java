3ded6203871f72f57f42664c8bb5411dcc5655ef
LuNoX <maillunox@gmail.com>
2016-10-11 20:29:37 +0200

    Updated CommandStorage by fixing a typo

--- src/utility/CommandStorage.java
+++ src/utility/CommandStorage.java
@@ -6,7 +6,7 @@ public class CommandStorage
 	protected String window_hBoost = "window_h_boost_";
 	protected String window_vBoost = "window_v_boost_";
 	protected String window_hBoostConst = "window_h_boost_is_const_";
-	protected String window_vBoostConst = "window_b_boost_is_const_";
+	protected String window_vBoostConst = "window_v_boost_is_const_";
 	protected String window_hasSfx = "window_has_sfx_";
 	protected String window_sfx = "window_sfx_";
 	protected String window_sfxFrame = "window_sfx_frame_";
