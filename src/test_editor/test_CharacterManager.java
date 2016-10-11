58643abbc6972f3ef218bef06e9cbc09c60de5d5
LuNoX <maillunox@gmail.com>
2016-10-11 22:18:17 +0200

    Updated for testing of new implementations and bug-fixing

--- src/test_editor/test_CharacterManager.java
+++ src/test_editor/test_CharacterManager.java
@@ -8,6 +8,12 @@ public class test_CharacterManager
 	public static void main(String[] args)
 	{
 		IniFileChooser ifc = new IniFileChooser();
+		
+		/*String[] tmp = {"a", "b", "c" , "d"};
+		Utilities u = new Utilities();
+		tmp = u.removeEntry(tmp, 1);
+		System.out.print(tmp[0] + " " + tmp[1] + " " + tmp[2]);*/
+		
 		CharacterManager cm = new CharacterManager(ifc.chooseFile("C:\\Users\\Timo\\Desktop\\active\\custom_etalus.ini"));
 		
 		for (int i = 0; i < cm.getAllMoves()[4].getWm().getWindows().length; i++)
