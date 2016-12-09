05ca68a56cbc78afbb999f89e1ec1532241b8a22
LuNoX <maillunox@gmail.com>
2016-12-09 15:25:06 +0100

    Added descriptions to commands
    Commands in CommandStorage now have a description and an optional
    additional description. All commands are now static.

--- src/utility/CommandStorage.java
+++ src/utility/CommandStorage.java
@@ -1,18 +1,33 @@
 package utility;
 
+//Credit to u/yougblood for the command descriptions
+
 public class CommandStorage
 {
-	protected String window_length = "window_length_";
-	protected String window_hBoost = "window_h_boost_";
-	protected String window_vBoost = "window_v_boost_";
-	protected String window_hBoostConst = "window_h_boost_is_const_";
-	protected String window_vBoostConst = "window_v_boost_is_const_";
-	protected String window_hasSfx = "window_has_sfx_";
-	protected String window_sfx = "window_sfx_";
-	protected String window_sfxFrame = "window_sfx_frame_";
-	protected String window_whiffFrames = "window_whiff_frames_";
-	protected String window_invincibility = "invincibility_";
+	public static String window_length = "window_length_";
+	public static String window_length_description = "The number of frames in a certain window";
+	public static String window_hBoost = "window_h_boost_";
+	public static String window_hBoost_description = "The horizontal boost during that window";
+	public static String window_vBoost = "window_v_boost_";
+	public static String window_vBoost_description = "The vertical boost during that window";
+	public static String window_hBoostConst = "window_h_boost_is_const_";
+	public static String window_hBoostConst_description = "Whether the window_h_boost is just a one-time burst or is a consistent boost during the whole window";
+	public static String window_vBoostConst = "window_v_boost_is_const_";
+	public static String window_vBoostConst_description = "Whether the window_v_boost is just a one-time burst or is a consistent boost during the whole window";
+	public static String window_hasSfx = "window_has_sfx_";
+	public static String window_hasSfx_description = "Whether the window has a sound effect or not";
+	public static String window_sfx = "window_sfx_";
+	public static String window_sfx_description = "The sound effect for the window.";
+	public static String window_sfx_additional_description = "There is no list of sound effects, so if you want a specific sound, you'll need to copy the value from a move that already has the sound you want.";
+	public static String window_sfxFrame = "window_sfx_frame_";
+	public static String window_sfxFrame_description = "The frame of the window where the sound effect is played";
+	public static String window_whiffFrames = "window_whiff_frames_";
+	public static String window_whiffFrames_description = "The number of frames that the window will last if you haven't hit the opponent with the move yet";
+	public static String window_invincibility = "invincibility_";
+	public static String window_invincibility_description = "Determines the invincibility properties of the move";
+	public static String window_invincibility_additional_description = "0 - not invincible \n1 - fully invincible \n2 - invincible to projectiles only";
 	
+	//TODO remove these getters
 	public String getWindow_length()
 	{
 		return window_length;
