e6befd9d6a11f22c2fd6df4f810312e2401b9550
LuNoX <maillunox@gmail.com>
2016-12-11 18:55:19 +0100

    Moved methods

--- src/utility/CommandStorage.java
+++ src/utility/CommandStorage.java
@@ -1,14 +1,12 @@
 package utility;
 
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 // Credit to u/yougblood for the command descriptions
 
 public class CommandStorage
 {
     // TODO add description and additional description to all fields
+
+    // Window
     public static String windowLength = "window_length_";
     public static String windowLengthDescription = "The number of frames in a certain window";
     public static String windowHBoost = "window_h_boost_";
@@ -39,83 +37,108 @@ public class CommandStorage
     public static String windowInvincibilityAdditionalDescription
                     = "0 - not invincible \n1 - fully invincible \n2 - invincible to projectiles only";
 
+    public static String[] windowCommands =
+    {"This is a placeholder string representing the id of the window and is here for convenience of conversion between this Array and tempWindow Arrays",
+                    CommandStorage.windowLength, CommandStorage.windowHBoost,
+                    CommandStorage.windowVBoost, CommandStorage.windowHBoostConst,
+                    CommandStorage.windowVBoostConst, CommandStorage.windowHasSfx,
+                    CommandStorage.windowSfx, CommandStorage.windowSfxFrame,
+                    CommandStorage.windowWhiffFrames, CommandStorage.windowInvincibility};
+
+    // Move
     public static String projectileHSpeed = "projectile_h_speed";
     public static String projectileVSpeed = "projectile_v_speed";
     public static String projectileGravity = "projectile_gravity";
     public static String projectileLifetime = "projectile_lifetime";
 
-    public static String[] windowCommands = // Id is ALWAYS at index 0
-    {"id", CommandStorage.windowLength, CommandStorage.windowHBoost, CommandStorage.windowVBoost,
-                    CommandStorage.windowHBoostConst, CommandStorage.windowVBoostConst,
-                    CommandStorage.windowHasSfx, CommandStorage.windowSfx,
-                    CommandStorage.windowSfxFrame, CommandStorage.windowWhiffFrames,
-                    CommandStorage.windowInvincibility};
+    // General
+    public static String knockbackAdjustment = "knockback_adjustment";
+    public static String hitstunGravityAccel = "hitstun_gravity_accel";
+    public static String gravityAccel = "gravity_accel";
+    public static String fastFallSpeed = "fast_fall_speed";
+    public static String maxFallSpeed = "max_fall_speed";
+    public static String groundFriction = "ground_friction";
+    public static String airFriction = "air_friction";
+    public static String maxDblJumps = "max_dbl_jumps";
+    public static String maxJumpHSpeed = "max_jump_h_speed";
+    public static String dblJumpSpeed = "dbl_jump_speed";
+    public static String shortHopSpeed = "short_hop_speed";
+    public static String jumpSpeed = "jump_speed";
+    public static String airAccel = "air_accel";
+    public static String maxAirSpeed = "max_air_speed";
+    public static String rollBackwardSpeed = "roll_backward_speed";
+    public static String rollBackwardRecoveryTime = "roll_backward_recovery_time";
+    public static String rollBackwardActiveTime = "roll_backward_active_time";
+    public static String rollBackwardStartupTime = "roll_backward_startup_time";
+    public static String rollForwardSpeed = "roll_forward_speed";
+    public static String rollForwardRecoveryTime = "roll_forward_recovery_time";
+    public static String rollForwardActiveTime = "roll_forward_active_time";
+    public static String rollForwardStartupTime = "roll_forward_startup_time";
+    public static String airDodgeSpeed = "air_dodge_speed";
+    public static String airDodgeRecoveryTime = "air_dodge_recovery_time";
+    public static String airDodgeActiveTime = "air_dodge_active_time";
+    public static String airDodgeStartupTime = "air_dodge_startup_time";
+    public static String techrollSpeed = "techroll_speed";
+    public static String techrollRecoveryTime = "techroll_recovery_time";
+    public static String techrollActiveTime = "techroll_active_time";
+    public static String techrollStartupTime = "techroll_startup_time";
+    public static String techRecoveryTime = "tech_recovery_time";
+    public static String techActiveTime = "tech_active_time";
+    public static String parryRecoveryTime = "parry_recovery_time";
+    public static String parryActiveTime = "parry_active_time";
+    public static String parryStartupTime = "parry_startup_time";
+    public static String waveLandFriction = "wave_land_friction";
+    public static String waveLandTime = "wave_land_time";
+    public static String jumpStartTime = "jump_start_time";
+    public static String pratFallAccel = "prat_fall_accel";
+    public static String pratLandTime = "prat_land_time";
+    public static String landTime = "land_time";
+    public static String initialDashSpeed = "initial_dash_speed";
+    public static String initialDashAccel = "initial_dash_accel";
+    public static String initialDashTime = "initial_dash_time";
+    public static String dashStopFriction = "dash_stop_friction";
+    public static String dashStopTime = "dash_stop_time";
+    public static String walljumpVSpeed = "walljump_v_speed";
+    public static String walljumpHSpeed = "walljump_h_speed";
+    public static String dashTurnAccel = "dash_turn_accel";
+    public static String dashTurnTime = "dash_turn_time";
+    public static String dashSpeed = "dash_speed";
+    public static String walkTurnTime = "walk_turn_time";
+    public static String walkAccel = "walk_accel";
+    public static String walkSpeed = "walk_speed";
 
+    public static String[] generalCommands =
+    {CommandStorage.knockbackAdjustment, CommandStorage.hitstunGravityAccel,
+                    CommandStorage.gravityAccel, CommandStorage.fastFallSpeed,
+                    CommandStorage.maxFallSpeed, CommandStorage.groundFriction,
+                    CommandStorage.airFriction, CommandStorage.maxDblJumps,
+                    CommandStorage.maxJumpHSpeed, CommandStorage.dblJumpSpeed,
+                    CommandStorage.shortHopSpeed, CommandStorage.jumpSpeed, CommandStorage.airAccel,
+                    CommandStorage.maxAirSpeed, CommandStorage.rollBackwardSpeed,
+                    CommandStorage.rollBackwardRecoveryTime, CommandStorage.rollBackwardActiveTime,
+                    CommandStorage.rollBackwardStartupTime, CommandStorage.rollForwardSpeed,
+                    CommandStorage.rollForwardRecoveryTime, CommandStorage.rollForwardActiveTime,
+                    CommandStorage.rollForwardStartupTime, CommandStorage.airDodgeSpeed,
+                    CommandStorage.airDodgeRecoveryTime, CommandStorage.airDodgeActiveTime,
+                    CommandStorage.airDodgeStartupTime, CommandStorage.techrollSpeed,
+                    CommandStorage.techrollRecoveryTime, CommandStorage.techrollActiveTime,
+                    CommandStorage.techrollStartupTime, CommandStorage.techRecoveryTime,
+                    CommandStorage.techActiveTime, CommandStorage.parryRecoveryTime,
+                    CommandStorage.parryActiveTime, CommandStorage.parryStartupTime,
+                    CommandStorage.waveLandFriction, CommandStorage.waveLandTime,
+                    CommandStorage.jumpStartTime, CommandStorage.pratFallAccel,
+                    CommandStorage.pratLandTime, CommandStorage.landTime,
+                    CommandStorage.initialDashSpeed, CommandStorage.initialDashAccel,
+                    CommandStorage.initialDashTime, CommandStorage.dashStopFriction,
+                    CommandStorage.dashStopTime, CommandStorage.walljumpVSpeed,
+                    CommandStorage.walljumpHSpeed, CommandStorage.dashTurnAccel,
+                    CommandStorage.dashTurnTime, CommandStorage.dashSpeed,
+                    CommandStorage.walkTurnTime, CommandStorage.walkAccel,
+                    CommandStorage.walkSpeed};
+
+    // Other
     public static String[] duplicateCommands =
     {CommandStorage.windowHBoost, CommandStorage.windowHBoostConst, CommandStorage.windowVBoost,
                     CommandStorage.windowVBoostConst, CommandStorage.windowHasSfx,
                     CommandStorage.windowSfx, CommandStorage.windowSfxFrame};
-
-    public static boolean isSpecificCommand(String toBeChecked, String command)
-    {
-        if (!toBeChecked.contains(command))
-        {
-
-            return false;
-        }
-        for (int i = 0; i < CommandStorage.duplicateCommands.length; i++)
-        {
-            if (!CommandStorage.duplicateCommands[i].equals(command) && toBeChecked.contains(
-                            CommandStorage.duplicateCommands[i]))
-            {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    public static List<float[]> addTempWindowCommand(List<float[]> listToBeAddedTo,
-                    String commandToBeAdded, int commandPosition)
-    {
-        List<float[]> result = listToBeAddedTo;
-        String[] windowCommands = CommandStorage.windowCommands;
-
-        int tmp = commandToBeAdded.indexOf(windowCommands[commandPosition])
-                        + windowCommands[commandPosition].length();
-        int id = Integer.parseInt(commandToBeAdded.substring(tmp, tmp + 1));
-
-        Pattern p = Pattern.compile("\"([^\"]*)\"");
-        Matcher m = p.matcher(commandToBeAdded);
-        m.find();
-        Float value = Float.parseFloat(m.group(1));
-
-        boolean belongsToExistingWindow = false;
-        for (float[] window : result)
-        {
-            if (id == window[0])
-            {
-                window[commandPosition] = value;
-                belongsToExistingWindow = true;
-            }
-        }
-        if (!belongsToExistingWindow)
-        {
-            float[] window = new float[windowCommands.length];
-            window[0] = id;
-            for (int i = 1; i < window.length; i++)
-            {
-                if (i == commandPosition)
-                {
-                    window[i] = value;
-                }
-                else
-                {
-                    window[i] = 0;
-                }
-            }
-            result.add(window);
-        }
-
-        return result;
-    }
 }
--- src/utility/Utilities.java
+++ src/utility/Utilities.java
@@ -1,5 +1,9 @@
 package utility;
 
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Utilities
 {
     public static int getStringPositionInArray(String searchForThis, String[] searchThroughThis)
@@ -30,6 +34,68 @@ public class Utilities
                 tmp = 1;
             }
             result[i] = arrayToRemoveFrom[i + tmp];
+        }
+
+        return result;
+    }
+    
+    public static boolean isSpecificCommand(String toBeChecked, String command)
+    {
+        if (!toBeChecked.contains(command))
+        {
+            return false;
+        }
+        for (int i = 0; i < CommandStorage.duplicateCommands.length; i++)
+        {
+            if (!CommandStorage.duplicateCommands[i].equals(command) && toBeChecked.contains(
+                            CommandStorage.duplicateCommands[i]))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static List<float[]> addTempWindowCommand(List<float[]> listToBeAddedTo,
+                    String commandToBeAdded, int commandPosition)
+    {
+        List<float[]> result = listToBeAddedTo;
+        String[] windowCommands = CommandStorage.windowCommands;
+
+        int tmp = commandToBeAdded.indexOf(windowCommands[commandPosition])
+                        + windowCommands[commandPosition].length();
+        int id = Integer.parseInt(commandToBeAdded.substring(tmp, tmp + 1));
+
+        Pattern p = Pattern.compile("\"([^\"]*)\"");
+        Matcher m = p.matcher(commandToBeAdded);
+        m.find();
+        Float value = Float.parseFloat(m.group(1));
+
+        boolean belongsToExistingWindow = false;
+        for (float[] window : result)
+        {
+            if (id == window[0])
+            {
+                window[commandPosition] = value;
+                belongsToExistingWindow = true;
+            }
+        }
+        if (!belongsToExistingWindow)
+        {
+            float[] window = new float[windowCommands.length];
+            window[0] = id;
+            for (int i = 1; i < window.length; i++)
+            {
+                if (i == commandPosition)
+                {
+                    window[i] = value;
+                }
+                else
+                {
+                    window[i] = 0;
+                }
+            }
+            result.add(window);
         }
 
         return result;
