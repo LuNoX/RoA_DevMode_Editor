0fd37a9877a33bf39732a8a44facfb876763e078
LuNoX <maillunox@gmail.com>
2016-12-11 21:31:21 +0100

    Removed TODOs

--- src/test/test_CharacterGeneral.java
+++ src/test/test_CharacterGeneral.java
@@ -8,7 +8,6 @@ public class test_CharacterGeneral
 
     public static void main(String[] args)
     {
-        // TODO Auto-generated method stub
         RoAFileChooser ifc = new RoAFileChooser();
 
         RoACharacter cm = new RoACharacter(ifc.chooseFile(
