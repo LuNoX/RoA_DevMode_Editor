00cfa125603fc66b7d8fd90f372ac53471589cf2
LuNoX <maillunox@gmail.com>
2017-01-17 20:37:57 +0100

    Projectiles now remove their specific commands from this.other

--- src/moves/Projectile.java
+++ src/moves/Projectile.java
@@ -1,10 +1,12 @@
 package moves;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import utility.CommandStorage;
+import utility.Utilities;
 
 public class Projectile extends Move
 {
@@ -17,36 +19,69 @@ public class Projectile extends Move
     {
         super(name, code);
         this.isProjectile = true;
-
+        this.initializeProjectileValues();
+    }
+    
+    public void initializeProjectileValues()
+    {
+        int numberOfCommands = 0;
+        int[] commandPositions = new int[this.other.size()];
+        
         Pattern p = Pattern.compile("\"([^\"]*)\"");
         Matcher m = null;
-
-        for (String command : this.other)
+        
+        for (int i = 0; i < this.other.size(); i++)
         {
-            if (command.contains(CommandStorage.projectileHSpeed))
+            String command = this.other.get(i);
+            
+            if(Utilities.isSpecificCommand(command, CommandStorage.projectileHSpeed))
             {
                 m = p.matcher(command);
                 m.find();
-                this.projectileLifetime = Float.parseFloat(m.group(1));
+                float value = Float.parseFloat(m.group(1));
+                this.projectileHSpeed = value;
+                
+                numberOfCommands++;
+                commandPositions[numberOfCommands - 1] = i;
             }
-            else if (command.contains(CommandStorage.projectileVSpeed))
+            else if (Utilities.isSpecificCommand(command, CommandStorage.projectileVSpeed))
             {
                 m = p.matcher(command);
                 m.find();
-                this.projectileLifetime = Float.parseFloat(m.group(1));
+                float value = Float.parseFloat(m.group(1));
+                this.projectileVSpeed = value;
+                
+                numberOfCommands++;
+                commandPositions[numberOfCommands - 1] = i;
             }
-            else if (command.contains(CommandStorage.projectileGravity))
+            else if (Utilities.isSpecificCommand(command, CommandStorage.projectileGravity))
             {
                 m = p.matcher(command);
                 m.find();
-                this.projectileLifetime = Float.parseFloat(m.group(1));
+                float value = Float.parseFloat(m.group(1));
+                this.projectileGravity = value;
+                
+                numberOfCommands++;
+                commandPositions[numberOfCommands - 1] = i;
             }
-            if (command.contains(CommandStorage.projectileLifetime))
+            else if (Utilities.isSpecificCommand(command, CommandStorage.projectileLifetime))
             {
                 m = p.matcher(command);
                 m.find();
-                this.projectileLifetime = Float.parseFloat(m.group(1));
+                float value = Float.parseFloat(m.group(1));
+                this.projectileLifetime = value;
+                
+                numberOfCommands++;
+                commandPositions[numberOfCommands - 1] = i;
             }
+        }
+        // remove the commands from the code
+        Arrays.sort(commandPositions); // sort the indices and then go through the list backwards to
+                                       // avoid index errors
+        for (int i = numberOfCommands - 1; i >= 0; i--) // dont use commandPositions.length because
+                                                        // the Array is far longer than needed
+        {
+            this.other.remove(commandPositions[i]);
         }
     }
 }
